414925565a5389e32fff092c4dc3d04283c4f4e7
MGiordano98 <maximo98.mg@gmail.com>
2020-04-05 15:01:43 +0200

    del max

--- Max/mycollective.h
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<mpi.h>
-
-void MyBroadcasting(int world_size, int world_ranks, int *numbers, int size);
-void MyGathering(int world_size, int world_ranks, int number);
-void MyScatter(int world_size, int world_ranks, int *numbers, int size);
