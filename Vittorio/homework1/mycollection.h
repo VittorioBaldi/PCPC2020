42b1d0a0d8a3406f6735bbfb6d3de55637c20092
MGiordano98 <maximo98.mg@gmail.com>
2020-04-05 15:05:10 +0200

    refactoring max branch

--- Vittorio/homework1/mycollection.h
+++
@@ -1,5 +0,0 @@
-void broadcasting(void* data, int count, int source, MPI_Datatype datatype, MPI_Comm comunicator);
-
-void scatter(int* datavoid, int count, int source, MPI_Datatype datatype, MPI_Comm comunicator, int *x);
-
-void gathering(int* data, int count, int source, MPI_Datatype datatype, MPI_Comm comunicator, int* data_source);
--- Zioposty/esercizi 2/mycollective.h
+++
@@ -1,5 +0,0 @@
-#define N 35
-
-void broadcast(int rank, int world_size, int *buffer, int size);
-void gathering(int rank, int world_size);
-void scattering(int rank, int world_size);
